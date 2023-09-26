a01e63433f25d606fae1e9427dbd6f94a0849253
rahulrk7426 <141915536+rahulrk7426@users.noreply.github.com>
2023-09-26 13:14:09 +0530

    Delete schoolstore/schoolproject directory

--- schoolstore/schoolproject/schoolapp/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class scchoolstore(models.Model):
-    name=models.CharField(max_length=250)
-    image=models.ImageField(upload_to='pic')
-    description=models.TextField()
